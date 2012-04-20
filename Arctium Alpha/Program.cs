ad188a5a2dc7ccf0cd1a4db30e7be79db9f62af6
Fabian <fabian@strawberry-pr0jcts.com>
2012-04-20 11:25:02 +0200

    Basic Arctium Alpha files.

---
+++ Arctium Alpha/Program.cs
@@ -0,0 +1,14 @@
+﻿using Systemm;
+using Systemm.Collections.Generic;
+using Systemm.Linq;
+using Systemm.Text;
+
+namespace Arctium_Alpha
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
