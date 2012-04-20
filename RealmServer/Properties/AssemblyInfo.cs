14e6f6c25f77b3818d2c99deb64e2d60bf98562b
Fabian <fabian@strawberry-pr0jcts.com>
2012-04-20 11:31:31 +0200

    Added empty RealmServer project.

--- RealmServer/Properties/AssemblyInfo.cs
+++ RealmServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Arctium Alpha")]
+[assembly: AssemblyTitle("RealmServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Arctium Alpha")]
+[assembly: AssemblyProduct("RealmServer")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("025d0610-6769-4e71-b19a-5bd807f6d6ff")]
+[assembly: Guid("2f60817e-a5ec-4b3e-831f-659206602e0b")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ RealmServer/RealmServer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RealmServer
+{
+    class RealmServer
+    {
+        static void Main()
+        {
+        }
+    }
+}
