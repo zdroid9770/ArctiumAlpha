a1e381edaadad8b9a7da5918f82b443ed5a45b26
Fabian <fabian@strawberry-pr0jcts.com>
2012-05-11 21:06:09 +0200

    Updated ServerEncryptionKey and ServerDecryptionKey.

--- Common/Cryptography/PacketCrypt.cs
+++ Common/Cryptography/PacketCrypt.cs
@@ -8,44 +8,15 @@ namespace Common.Cryptography
 {
     public class PacketCrypt
     {
-        /// <summary>
-        /// The amount of bytes to drop from the stream initially.
-        /// 
-        /// This is to resist the FMS attack.
-        /// </summary>
         public const int DropN = 1024;
 
-        /// <summary>
-        /// This is the key the client uses to encrypt its packets
-        /// This is also the key the server uses to decrypt the packets
-        /// </summary>
-        private static readonly byte[] ServerDecryptionKey =
-                        {
-                                0xC2, 0xB3, 0x72, 0x3C, 0xC6, 0xAE, 0xD9, 0xB5,
-                                0x34, 0x3C, 0x53, 0xEE, 0x2F, 0x43, 0x67, 0xCE
-                        };
+        private static readonly byte[] ServerDecryptionKey = { 0x40, 0xAA, 0xD3, 0x92, 0x26, 0x71, 0x43, 0x47, 0x3A, 0x31, 0x08, 0xA6, 0xE7, 0xDC, 0x98, 0x2A };
+        private static readonly byte[] ServerEncryptionKey = { 0x08, 0xF1, 0x95, 0x9F, 0x47, 0xE5, 0xD2, 0xDB, 0xA1, 0x3D, 0x77, 0x8F, 0x3F, 0x3E, 0xE7, 0x00 };
 
-        /// <summary>
-        /// This is the key the client uses to decrypt server packets
-        /// This is also the key the server uses to encrypt the packets
-        /// </summary>
-        private static readonly byte[] ServerEncryptionKey =
-                        {
-                                0xCC, 0x98, 0xAE, 0x04, 0xE8, 0x97, 0xEA, 0xCA,
-                                0x12, 0xDD, 0xC0, 0x93, 0x42, 0x91, 0x53, 0x57
-                        };
-
-        // This is valid as HMAC-SHA1 transforms can be reused
         static readonly HMACSHA1 s_decryptClientDataHMAC = new HMACSHA1(ServerDecryptionKey);
         static readonly HMACSHA1 s_encryptServerDataHMAC = new HMACSHA1(ServerEncryptionKey);
 
-        /// <summary>
-        /// Encrypts data sent to the client
-        /// </summary>
         private readonly ARC4 encryptServerData;
-        /// <summary>
-        /// Decrypts data sent from the client
-        /// </summary>
         private readonly ARC4 decryptClientData;
 
         public PacketCrypt(byte[] sessionKey)
@@ -53,23 +24,13 @@ namespace Common.Cryptography
             var encryptHash = s_encryptServerDataHMAC.ComputeHash(sessionKey);
             var decryptHash = s_decryptClientDataHMAC.ComputeHash(sessionKey);
 
-            // Used by the client to decrypt packets sent by the server
-            //var decryptServerData = new ARC4(encryptHash); // CLIENT-SIDE
-            // Used by the server to decrypt packets sent by the client
-            decryptClientData = new ARC4(decryptHash); // SERVER-SIDE
-            // Used by the server to encrypt packets sent to the client
-            encryptServerData = new ARC4(encryptHash); // SERVER-SIDE
-            // Used by the client to encrypt packets sent to the server
-            //var encryptClientData = new ARC4(decryptHash); // CLIENT-SIDE
+            decryptClientData = new ARC4(decryptHash);
+            encryptServerData = new ARC4(encryptHash);
 
-            // Use the 2 encryption objects to generate a common starting point
             var syncBuffer = new byte[DropN];
             encryptServerData.Process(syncBuffer, 0, syncBuffer.Length);
-            //encryptClientData.Process(syncBuffer, 0, syncBuffer.Length);
 
-            // Use the 2 decryption objects to generate a common starting point
             syncBuffer = new byte[DropN];
-            //decryptServerData.Process(syncBuffer, 0, syncBuffer.Length);
             decryptClientData.Process(syncBuffer, 0, syncBuffer.Length);
         }
 
